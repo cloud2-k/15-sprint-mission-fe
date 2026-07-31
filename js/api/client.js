273bc1e71db377132be86958c1b5444b7374f566
cloud2-k <susie.k.works@gmail.com>
2026-07-31 22:50:31 +0900

    feat: API 동작 검증을 위한 테스트 구현 (main.js)

--- js/api/client.js
+++ js/api/client.js
@@ -2,7 +2,7 @@
  * @file 전역 Axios 인스턴스 설정 파일
  * @description API 서버와의 통신을 위한 기본 설정(baseURL, timeout 등)이 적용된 인스턴스를 생성하고 관리합니다.
  * @author 김수지
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 // import axios from "axios";
@@ -10,5 +10,5 @@ import axios from "https://cdn.jsdelivr.net/npm/axios/+esm";
 
 export const instance = axios.create({
   baseURL: "https://panda-market-api-crud.vercel.app",
-  timeout: 3000,
+  timeout: 10000,
 });
